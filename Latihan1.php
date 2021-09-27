1da7d6a16a124a1e22b45e26a6444cbdf582e8de
idlavirm <91013511+idlavirm@users.noreply.github.com>
2021-09-27 15:32:41 +0700

    Update Latihan1.php

--- Latihan1.php
+++ Latihan1.php
@@ -5,7 +5,11 @@
     }      public function penjumlahan($n1, $n2) 
     { 
         $this->load->model('Model_latihan1'); 
-        $hasil = $this->Model_latihan1->jumlah($n1, $n2);         
-        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil; 
+ 
+        $data['nilai1'] = $n1; 
+        $data['nilai2'] = $n2; 
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);  
+        $this->load->view('view_latihan', $data); 
     } 
 } 
+
